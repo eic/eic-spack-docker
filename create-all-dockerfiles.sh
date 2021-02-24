23176580c61f3cbff62f57920942183ccafc2097
Wouter Deconinck <wdconinc@gmail.com>
2021-02-23 20:47:32 -0600

    Ubuntu 19.04 and 19.10 are EOL

--- create-all-dockerfiles.sh
+++ create-all-dockerfiles.sh
@@ -2,6 +2,6 @@
 
 dir=`dirname $0`
 
-for os in centos:7 centos:8 ubuntu:18.04 ubuntu:19.04 ubuntu:20.04 ubuntu:20.10 ; do
+for os in centos:7 centos:8 ubuntu:18.04 ubuntu:20.04 ubuntu:20.10 ; do
   ${dir}/create-one-dockerfile.sh ${os}
 done
